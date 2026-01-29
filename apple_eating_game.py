d34aa69d2cb418e4f96e2bc42086e0f823f9d368
kazak <kazakova.katya@outlook.com>
2026-01-29 15:46:15 +0700

    refactored print_board & moves, added arrow keys controls

--- apple_eating_game.py
+++ apple_eating_game.py
@@ -2,16 +2,12 @@ import curses
 from time import sleep
 from random import randint
 
-def generate_apple():
-    x = randint(start_x + 1, board_width + start_x)
-    y = randint(start_y + 1, board_height + start_y)
+def main(stdscr):
+    global window, start_x, start_y, board_height, board_width, character_x, character_y
 
-    return x, y
-
-def main(window):
-    global start_x, start_y, board_height, board_width
-
+    window = stdscr
     curses.curs_set(0)
+    window.keypad(True)
 
     start_x, start_y = 2, 2
     board_width, board_height = 16, 8
@@ -26,24 +22,18 @@ def main(window):
 
     apple_x, apple_y = generate_apple()
 
-    super_apple_chance = 2
+    super_apple_probability = 2
     super_apple_x, super_apple_y = 0, 0
-    chance = 0
+    super_apple_chance = 0
 
     while True:
-        is_super_apple = chance == 1
         window.clear()
 
-        # print board
-        window.addstr(start_y, start_x, f"+{'-'*board_width}+")
-        window.addstr(start_y+1+board_height, start_x, f"+{'-'*board_width}+")
-        for row in range(1, board_height+1):
-            window.addstr(start_y+row, start_x, f"|")
-            window.addstr(start_y+row, start_x+board_width+1, f"|")
+        print_board()
 
         window.addch(apple_y, apple_x, apple)
 
-        if is_super_apple:
+        if super_apple_chance == 1:
             window.addstr(super_apple_y, super_apple_x, super_apple)
 
         window.addstr(character_y, character_x, character)
@@ -51,25 +41,21 @@ def main(window):
 
         window.addstr(0, 0, f"({character_x}, {character_y}) Score: {score}")
 
+        global pressed_key
         pressed_key = window.getch()
+
         if pressed_key == ord('q'):
             window.addstr(0, 0, 'thank you for playing the game')
             window.refresh()
             sleep(1)
             break
-        elif pressed_key == ord('w') and character_y > start_y+1:
-            character_y -= 1
-        elif pressed_key == ord('r') and character_y < start_y+board_height:
-            character_y += 1
-        elif pressed_key == ord('a') and character_x > start_x+1:
-            character_x -= 1
-        elif pressed_key == ord('s') and character_x < start_x+board_width:
-            character_x += 1
+
+        move()
 
         if character_x == apple_x and character_y == apple_y:
             apple_x, apple_y = generate_apple()
-            chance = randint(1, super_apple_chance)
-            if chance == 1:
+            super_apple_chance = randint(1, super_apple_probability)
+            if super_apple_chance == 1:
                 super_apple_x, super_apple_y = generate_apple()
 
             score += 1
@@ -77,8 +63,33 @@ def main(window):
         if character_x == super_apple_x and character_y == super_apple_y:
             apple_x, apple_y = generate_apple()
             score += 10
-            chance = 0
+            super_apple_chance = 0
             super_apple_x = super_apple_y = 0
+
+def print_board():
+    window.addstr(start_y, start_x, f"+{'-' * board_width}+")
+    window.addstr(start_y + 1 + board_height, start_x, f"+{'-' * board_width}+")
+    for row in range(1, board_height + 1):
+        window.addstr(start_y + row, start_x, f"|")
+        window.addstr(start_y + row, start_x + board_width + 1, f"|")
+
+def move():
+    global character_x, character_y
+
+    if (pressed_key == ord('w') or pressed_key == curses.KEY_UP) and character_y > start_y + 1:
+        character_y -= 1
+    elif (pressed_key == ord('r') or pressed_key == curses.KEY_DOWN) and character_y < start_y + board_height:
+        character_y += 1
+    elif (pressed_key == ord('a') or pressed_key == curses.KEY_LEFT) and character_x > start_x + 1:
+        character_x -= 1
+    elif (pressed_key == ord('s') or pressed_key == curses.KEY_RIGHT) and character_x < start_x + board_width:
+        character_x += 1
+
+def generate_apple():
+    x = randint(start_x + 1, board_width + start_x)
+    y = randint(start_y + 1, board_height + start_y)
+
+    return x, y
 
 
 curses.wrapper(main)
