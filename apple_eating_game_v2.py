268eeda486776da6b521599555b199d5c10efe2c
kazak <kazakova.katya@outlook.com>
2026-01-30 00:22:57 +0700

    refactored v2 to popup window board

--- apple_eating_game_v2.py
+++ apple_eating_game_v2.py
@@ -6,7 +6,7 @@ from random import randint
 def main(stdscr):
     global window, start_x, start_y, board_height, board_width, \
         character_x, character_y, score, character, \
-        apple, super_apple, ready
+        apple, super_apple, ready, board
 
     window = stdscr
 
@@ -26,11 +26,11 @@ def main(stdscr):
     curses.noecho()
     window.nodelay(True)
 
-    start_x, start_y = 2, 3
-    board_width, board_height = 16, 8
+    start_x, start_y = 1, 3
+    board_width, board_height = 17, 7
 
-    character_x = start_x + board_width//2
-    character_y = start_y + board_height//2
+    character_x = board_width//2 + 1
+    character_y = board_height//2 + 1
     score = 0
 
     apple_x, apple_y = generate_apple()
@@ -42,9 +42,8 @@ def main(stdscr):
     timer = 30.0
 
     window.clear()
-    print_board()
+    board = create_board()
     print_leaderboard()
-    window.addstr(24, 0, f'{apple_y, apple_x}')
 
     previous_time = monotonic()
     while timer > 0:
@@ -54,31 +53,33 @@ def main(stdscr):
         timer -= time_difference
 
         # add apple(s)
-        window.addstr(apple_y, apple_x, apple)
+        board.addstr(apple_y, apple_x, apple)
         if super_apple_chance == 1:
-            window.addstr(super_apple_y, super_apple_x, super_apple)
+            board.addstr(super_apple_y, super_apple_x, super_apple)
 
         # add player character
-        window.addstr(character_y, character_x, character)
+        board.addstr(character_y, character_x, character)
 
         # add score & timer
         window.addstr(0, 0, f"Your Score: {score}")
         window.addstr(1, 0, f'Remaining Time: {timer: .2f}  ')
 
-        window.refresh()  # display everything
+        window.refresh()
+        board.refresh()  # display everything
 
         global pressed_key
-        pressed_key = window.getch()
+        pressed_key = board.getch()
         if pressed_key == ord('q'):
+            window.clear()
             window.addstr(0, 0, 'thank you for playing the game')
             window.refresh()
-            sleep(0.5)
-            break
+            sleep(1)
+            return
         move()
 
         if character_x == apple_x and character_y == apple_y:
-            window.addstr(apple_y, apple_x, ' ')  # erase apple visual
-            if super_apple_x != 0 and super_apple_y != 0: window.addstr(super_apple_y, super_apple_x, ' ')  # erase super apple visual
+            board.addstr(apple_y, apple_x, ' ')  # erase apple visual
+            if super_apple_x != 0 and super_apple_y != 0: board.addstr(super_apple_y, super_apple_x, ' ')  # erase super apple visual
 
             apple_x, apple_y = generate_apple()
             super_apple_chance = randint(1, super_apple_probability)
@@ -87,8 +88,8 @@ def main(stdscr):
             score += 1
 
         if character_x == super_apple_x and character_y == super_apple_y:
-            window.addstr(super_apple_y, super_apple_x, ' ')  # erase super apple visual
-            window.addstr(apple_y, apple_x, ' ')  # erase apple visual
+            board.addstr(super_apple_y, super_apple_x, ' ')  # erase super apple visual
+            board.addstr(apple_y, apple_x, ' ')  # erase apple visual
 
             apple_x, apple_y = generate_apple()
             score += 10
@@ -97,9 +98,9 @@ def main(stdscr):
 
         sleep(0.016)
 
-    curses.echo()
     timeout_popup()
 
+    curses.echo()
     window.nodelay(False)
     curses.curs_set(1)
 
@@ -136,6 +137,12 @@ def starting_menu():
         ready_byte = window.getstr(6, len(ready_prompt), 1)
         ready = ready_byte.decode('utf-8')
 
+def create_board():
+    board = curses.newwin(board_height+2, board_width+2, start_y, start_x)
+    board.box()
+    board.nodelay(True)
+    return board
+
 def print_board():
     window.addstr(start_y, start_x, f"+{'-' * board_width}+")
     window.addstr(start_y + 1 + board_height, start_x, f"+{'-' * board_width}+")
@@ -153,23 +160,23 @@ def print_leaderboard():
 
     for player in range(10):
         try:
-            window.addstr(start_y+1+player, leaderboard_xlocation, f'{scores[player].split()[0]:11s}{scores[player].strip().split()[1]}')
+            window.addstr(start_y+1+player, leaderboard_xlocation, f'{scores[player].split()[0]:12s}{scores[player].strip().split()[1]}')
         except:
             print()
 
 def move():
-    global character_x, character_y
+    global character_x, character_y, board
 
-    window.addstr(character_y, character_x, ' ')
+    board.addstr(character_y, character_x, ' ')
 
-    if (pressed_key == ord('w') or pressed_key == curses.KEY_UP) and character_y > start_y + 1:
+    if (pressed_key == ord('w') or pressed_key == curses.KEY_UP) and character_y > 1:
         character_y -= 1
-    elif (pressed_key == ord('r') or pressed_key == curses.KEY_DOWN) and character_y < start_y + board_height:
+    elif (pressed_key == ord('r') or pressed_key == curses.KEY_DOWN) and character_y < board_height:
         character_y += 1
-    elif (pressed_key == ord('a') or pressed_key == curses.KEY_LEFT) and character_x > start_x + 1:
-        character_x -= 1
-    elif (pressed_key == ord('s') or pressed_key == curses.KEY_RIGHT) and character_x < start_x + board_width:
-        character_x += 1
+    elif (pressed_key == ord('a') or pressed_key == curses.KEY_LEFT) and character_x > 1:
+        character_x -= 2
+    elif (pressed_key == ord('s') or pressed_key == curses.KEY_RIGHT) and character_x < board_width:
+        character_x += 2
 
 def save_score(name, score):
     scoreboard = open('scoreboard.txt', mode='a')
@@ -178,18 +185,20 @@ def save_score(name, score):
     sort_leaderboard()
 
 def generate_apple():
-    x = randint(start_x + 1, board_width + start_x)
-    y = randint(start_y + 1, board_height + start_y)
+
+    x = 0
+    while x % 2 == 0: x = randint(1, board_width)
+    y = randint(1, board_height)
 
     return x, y
 
 def timeout_popup():
-    popup_width, popup_height = 20, 5
+    popup_width, popup_height = board_width+4, 3
 
-    popup = curses.newwin(popup_height, popup_width, 2, start_x-1)
+    popup = curses.newwin(popup_height, popup_width, start_y+(board_height//2-popup_height//2)+1, start_x-1)
     popup.box()
-    timeout_text = 'Time out!!'
-    popup.addstr(popup_height//2, (popup_width-len(timeout_text))//2, timeout_text)
+    timeout_text = 'Time\'s out!'
+    popup.addstr(popup_height//2, popup_width//2-len(timeout_text)//2, timeout_text)
     popup.refresh()
     sleep(3)
 
