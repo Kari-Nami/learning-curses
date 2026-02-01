daaad9e9ec192811c72192838d4af2d9e20168a9
kazak <kazakova.katya@outlook.com>
2026-02-01 22:34:35 +0700

    refactored directions, added next location tracking

--- snake_game/snake_v1.py
+++ snake_game/snake_v1.py
@@ -15,11 +15,15 @@ def main(window):
 
     body = '##'
     snake_length = 1
-    current_rotation = -1  # 0 = u, 1 = r, 2 = d, 3 = l
+
+    #               up     right    down    left
+    directions = [(0, -1), (2, 0), (0, 1), (-2, 0)]
+    current_rotation = -1
+    next_x, next_y = None, None
 
     window.refresh()
 
-    board = curses.newwin(board_h, board_w*2, 1, 0)
+    board = curses.newwin(board_h, board_w*2, 2, 0)
     curses.curs_set(0)
     board.nodelay(True)
     board.keypad(True)
@@ -47,30 +51,28 @@ def main(window):
             snake_length = 1
 
         # w
-        elif key == ord('w') and player_y>1:
-            current_rotation = 0
+        elif key == ord('w') and player_y>1: current_rotation = 0
 
         # d
-        elif key == ord('s') and player_x<board_w*2-3:
-            current_rotation = 1
+        elif key == ord('s') and player_x<board_w*2-3: current_rotation = 1
 
         # s
-        elif key == ord('r') and player_y<board_h-2:
-            current_rotation = 2
+        elif key == ord('r') and player_y<board_h-2: current_rotation = 2
 
         # a
-        elif key == ord('a') and player_x>1:
-            current_rotation = 3
+        elif key == ord('a') and player_x>1: current_rotation = 3
 
-        if current_rotation != -1 and 1<player_x<board_w*2-3 and  1<player_y<board_h-2:
-            if current_rotation == 0: player_y -= 1
-            elif current_rotation == 1: player_x += 2
-            elif current_rotation == 2: player_y += 1
-            elif current_rotation == 3: player_x -= 2
+        if current_rotation != -1:
+            next_x, next_y = player_x + directions[current_rotation][0], player_y + directions[current_rotation][1]
 
-            sleep(0.5)
+            if 0 < next_x < board_w*2-2  and 0 < next_y < board_h - 1:
+                player_x, player_y = next_x, next_y
 
-        sleep(0.016)
+            # sleep(0.5)
+
+        window.addstr(1, 0, f'current: {(player_x, player_y)}, next: {(next_x, next_y)}      ')
+
+        sleep(0.25)
 
 
 curses.wrapper(main)
